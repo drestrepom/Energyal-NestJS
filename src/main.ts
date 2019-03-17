f76b389ec58e67d5c1fd52a2d497f43f4606447d
skouro <restrepomesadiego@gmail.com>
2019-03-17 18:09:00 -0500

    Se completo el login y el registro de usuarios

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+  if (process.env.NODE_ENV === 'dev') {
+    process.env.urlDB = 'mongodb://localhost:27017/enrgyal';
+  } else {
+    process.env.urlDB = 'mongodb+srv://admin:1193120855@cluster0-xjwrt.mongodb.net/enrgyal?retryWrites=true';
+  }
   process.env.EXPITARION = process.env.EXPITARION || '12h';
   process.env.SEED = process.env.SEED || 'chucho';
   process.env.urlDB = process.env.urlDB || 'mongodb://localhost:27017/enrgyal';
